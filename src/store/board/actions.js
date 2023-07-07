fb98a4b0daa11621e751296a9d576e2410b291be
iknowca <iknowca@gmail.com>
2023-07-07 17:26:04 +0900

    upgrade: [김원기] put mapping을 post mapping으로 수정

--- src/store/board/actions.js
+++ src/store/board/actions.js
@@ -1,9 +1,8 @@
 import axiosSpring from "@/utility/axiosInst";
 
 export default {
-  requestPutBoard(context, payload) {
-    axiosSpring.put('/board/board', payload)
-      .then()
+  requestPostBoard(context, payload) {
+    axiosSpring.post('/board/board', payload)
       .catch(()=>alert(payload))
   },
 }
