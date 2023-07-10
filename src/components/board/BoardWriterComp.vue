d903f3160d728d49b7b0e748a487ba08d69a0dd8
iknowca <iknowca@gmail.com>
2023-07-10 15:33:16 +0900

    refactor: [김원기] BoardWriterVue 리팩토링 [IFA-FRONT-8]

---
+++ src/components/board/BoardWriterComp.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-card class="pa-7">
+      <v-select style="width: 200px" :items="area">
+      </v-select>
+      <v-text-field label="title" v-model="title"></v-text-field>
+      <v-textarea label="content" v-model="stringContent"></v-textarea>
+      <v-container>
+        <v-row>
+          <v-col cols="4"></v-col>
+          <v-col cols="2" @click="goToHome"><v-btn>cancel</v-btn></v-col>
+          <v-col cols="2" @click="submit"><v-btn>submit</v-btn></v-col>
+          <v-col cols="4"></v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+import router from "@/router";
+
+const BoardModule = 'BoardModule'
+
+export default {
+  name: "BoardWriterComp",
+  data() {
+    return {
+      title: '',
+      stringContent: '',
+      area: ['current area', 'home area', 'working area']
+    }
+  },
+  methods: {
+    submit() {
+      this.requestPostBoard({userToken: localStorage.getItem('userToken'),
+        title: this.title,
+        stringContent: this.stringContent})
+    },
+    ...mapActions(BoardModule, ['requestPostBoard']),
+    goToHome() {
+      router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/board/WriteView.vue
+++ src/views/board/WriteView.vue
@@ -1,48 +1,16 @@
 <template>
-  <v-container>
-    <v-card class="pa-7">
-        <v-select style="width: 200px" :items="area">
-        </v-select>
-      <v-text-field label="title" v-model="title"></v-text-field>
-      <v-textarea label="content" v-model="stringContent"></v-textarea>
-      <v-container>
-        <v-row>
-          <v-col cols="4"></v-col>
-          <v-col cols="2" @click="goToHome"><v-btn>cancel</v-btn></v-col>
-          <v-col cols="2" @click="submit"><v-btn>submit</v-btn></v-col>
-          <v-col cols="4"></v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-  </v-container>
+<board-writer-comp></board-writer-comp>
 </template>
 
 <script>
 
-import {mapActions} from "vuex";
-import router from "@/router";
 
-const BoardModule = 'BoardModule'
+import BoardWriterComp from "@/components/board/BoardWriterComp.vue";
+
 export default {
   name: "WriteView",
-  data() {
-    return {
-      title: '',
-      stringContent: '',
-      area: ['current area', 'home area', 'working area']
-    }
-  },
-  methods: {
-    submit() {
-      this.requestPostBoard({userToken: localStorage.getItem('userToken'),
-      title: this.title,
-      stringContent: this.stringContent})
-    },
-    ...mapActions(BoardModule, ['requestPostBoard']),
-    goToHome() {
-      router.push('/')
-    }
-  }
+  components: {BoardWriterComp},
+
 }
 </script>
 
