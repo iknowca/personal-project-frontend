6ba58936b50f548eae9ec99a31b3dab06ba9cc8f
iknowca <iknowca@gmail.com>
2023-07-13 09:45:58 +0900

    feat: [김원기] 게시글 이미지 첨부기능 추가 [IFA-FRONT-14]

--- src/components/board/BoardWriterComp.vue
+++ src/components/board/BoardWriterComp.vue
@@ -5,6 +5,8 @@
       </v-select>
       <v-text-field label="title" v-model="title"></v-text-field>
       <v-textarea label="content" v-model="stringContent"></v-textarea>
+      <v-file-input multiple v-model="files" @change="previewImage" chips/>
+      <v-row><v-col v-for="(url, i) in previewImages" :key=i class="xs12 sm6 md4 lg3 xl2 xxl1"><v-img :src="url"></v-img></v-col></v-row>
       <v-container>
         <v-row>
           <v-col cols="4"></v-col>
@@ -20,6 +22,7 @@
 <script>
 import {mapActions} from "vuex";
 import router from "@/router";
+import AWS from 'aws-sdk'
 
 const BoardModule = 'BoardModule'
 
@@ -29,18 +32,55 @@ export default {
     return {
       title: '',
       stringContent: '',
-      area: ['current area', 'home area', 'working area']
+      area: ['current area', 'home area', 'working area'],
+      files: [],
+      awsBucketName: process.env.VUE_APP_S3_BUCKET_NAME,
+      awsBucketRegion: process.env.VUE_APP_S3_REGION,
+      awsIdentityPoolId: process.env.VUE_APP_S3_IDENTITY_POOL_ID,
+      s3: null,
+      previewImages: [],
     }
   },
   methods: {
-    submit() {
-      this.requestPostBoard({userToken: localStorage.getItem('userToken'),
+    async submit() {
+      const payload = {userToken: localStorage.getItem('userToken'),
         title: this.title,
-        stringContent: this.stringContent})
+        stringContent: this.stringContent, files:this.files.map((file)=>file.name)}
+      this.s3FileUpload()
+      await this.requestPostBoard(payload)
     },
     ...mapActions(BoardModule, ['requestPostBoard']),
     goToHome() {
       router.push('/')
+    },
+    s3FileUpload() {
+      this.s3Config()
+      for(let i=0; i<this.files.length; i++) {
+        let file = this.files[i];
+        this.s3.upload({Key:file.name, Body: file, ACL:'public-read'}, (err) => {
+          if(err) {
+            console.log(err)
+            return alert("error: " + err)
+          }
+        })
+      }
+    },
+    s3Config() {
+      AWS.config.update({
+        region: this.awsBucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: this.awsIdentityPoolId
+        })
+      })
+      this.s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {
+          Bucket: this.awsBucketName
+        }
+      })
+    },
+    previewImage() {
+      this.previewImages = this.files.map((file)=>URL.createObjectURL(file))
     }
   }
 }
