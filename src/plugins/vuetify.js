2910d8a9ca44ed59b4b7ac53c98de8c2ef6516a8
iknowca <iknowca@gmail.com>
2023-07-30 15:26:59 +0900

    feat: [김원기] 테마구성 [IFA-FRONT-5]

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,26 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const lightTheme = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#F0c20A',
+    secondary: '#789e4f',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+export default createVuetify({
+    // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+    theme: {
+      defaultTheme: 'lightTheme',
+      themes: {
+        lightTheme,
+      },
+    },
+  }
 )
